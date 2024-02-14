1039425f5a4f7297ee08c2c429c2cbfce5d522b2
ByungjinKang <105633057+ByungjinKang@users.noreply.github.com>
2024-02-14 17:25:59 +0900

    oracle first version

---
+++ Sequences/SEQ_USER.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 수요일-2월-14-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence SEQ_USER
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "C##TRACKER"."SEQ_USER"  MINVALUE 1 MAXVALUE 99999 INCREMENT BY 1 START WITH 13 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
